57d4df9cb8b8b9635b78ea433b4a1c577991f326
Mehdi Trfhgx <42844286+trfhgx@users.noreply.github.com>
2024-12-28 13:47:28 +0100

    changed legacy code --not finished

--- src/model_LOF/LOF.h
+++ src/model_LOF/LOF.h
@@ -33,22 +33,10 @@ typedef struct Metadata
 } Metadata, *MetadataP;
 
 // exemple of how the physical should be in the case of one LOF file in it (Logical file)
-typedef struct LOF_MS
-{
-    FILE *f;    // Physical file (Physical fle)
-    Metadata H; // the metadata in main memory
-} LOF_Ms, *LOF_MsP;
+
 
 ////=================================== Primitives for LOF
-LOF_MsP LOF_open(LOF_MsP f, char *Fname, char mode);  //ouvrir le fichier logique
-void LOF_close(LOF_MsP f);  //fermer le fichier logique
-void writeMetadata(LOF_MsP f, int i, int val); //affecter la valeur val au i ème champ des Metadata
-int readMetadata(LOF_MsP f, int i);  //retourner le contenue du i ème champ de les Metadata
-void printMetadata(LOF_MsP f, char *Fname);  //afficher le contenue des Metadata
-void LOF_writeBlock(LOF_MsP f, int i, Buffer buffer);  //mettre le contenue du tbuffer dans le bloc numero K
-void LOF_readBlock(LOF_MsP f, int i, Buffer buffer);  //mettre le contenue du bloc numero i dans le buffer
-void allocBlock(LOF_MsP f, int* i, Buffer* buffer);   //allouer un nouveau bloc et l'initialiser avec le contenue du buffer
-void displayLOF(LOF_MsP f, char *Fname, char result[]);     //afficher le contenue du fichier
+// -------
 
 ////=================================== Block 
 void displayBlock(BlockP R); 
